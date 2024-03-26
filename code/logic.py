734611f879c6823511c7c0a656727470fcda4dce
Jonas Gilbert <jonas.gilbert@student.hs-rm.de>
2024-03-26 10:29:12 +0100

    evaluation of a turing machine

--- code/logic.py
+++ code/logic.py
@@ -46,3 +46,46 @@ class TuringMachine:
                 return False
 
         return True
+
+
+    def eval_turing_machine(self, state, position=0):
+        symbol_on_tape = self.tape[position]
+        for instruction in self.instructions:
+            if instruction[0][0] == state and symbol_on_tape == instruction[0][1]:
+                self.tape[position] = instruction[1][1]
+                state = instruction[1][0]
+
+                if position == 0 and instruction[1][2] == "L":
+                    self.tape.insert(0, "_")
+                elif position == len(self.tape)-1 and instruction[1][2] == "R":
+                    self.tape.append("_")
+                    position += 1
+                else:
+                    if instruction[1][2] == "L":
+                        position -= 1
+                    elif instruction[1][2] == "R":
+                        position += 1
+                break
+
+        if state == self.end_state:
+            return self.tape
+        else:
+            return self.eval_turing_machine(state, position)
+
+
+# example
+tape = [1, 0, 1, 0, 1, 1, 0, 0]
+alphabet = {"_", 0, 1}
+states = {"a", "b", "c", "d"}
+instructions = [(("a", 1), ("a", 1, "R")),
+                (("a", 0), ("a", 0, "R")),
+                (("a", "_"), ("d", "_", "L")),
+                (("c", 0), ("c", 0, "L")),
+                (("c", 1), ("c", 1, "L")),
+                (("c", "_"), ("b", "_", "R")),
+                (("d", 0), ("c", 1, "L")),
+                (("d", 1), ("d", 0, "L")),
+                (("d" "_"), ("b", 1, "N"))]
+
+tm = TuringMachine(alphabet, states, tape, instructions, "a", "b")
+print(tm.eval_turing_machine(tm.start_state, 0))
